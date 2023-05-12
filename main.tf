c2a566433b7dfaab6823461bb175a45cb4f2b75c
RajRonal <108928775+RajRonal@users.noreply.github.com>
2023-05-12 17:22:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1" # Replace with the desired region
-  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  access_key = "AKIATNS7C4QCP3NZJDRT"
+  secret_key = "4HYxhy/Udb8FuOCZ7ALhTeY6ySXOVGTDyo3jvWI1"
 }
 
 resource "aws_security_group_rule" "rds-user" {
