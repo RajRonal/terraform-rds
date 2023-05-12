d30ee44d7962a4e3e72d927be9ca22499872e684
RajRonal <ronal.raj@remotestate.com>
2023-05-12 17:17:41 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-south-1" # Replace with the desired region
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+}
+
+resource "aws_security_group_rule" "rds-user" {
+  type        = "ingress"
+  from_port   = -1 # Replace with the desired port number
+  to_port     = -1 # Replace with the desired port number
+  protocol    = "tcp"
+  cidr_blocks = ["122.161.52.113/32"] # Replace with the desired IP range
+  
+  security_group_id = "sg-0b0ddee46bf0703b4" # Replace with the desired security group ID
+}
